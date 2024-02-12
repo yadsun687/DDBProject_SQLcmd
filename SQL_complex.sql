2fbdeaba7a225f974288af0d101ff9f427ea8bd1
Alknot1122 <117248001+Alknot1122@users.noreply.github.com>
2024-02-12 22:30:29 +0700

    Update SQL_complex.sql

--- SQL_complex.sql
+++ SQL_complex.sql
@@ -1,14 +1,3 @@
--- หาว่า MANAGER แต่ละคนมีจำนวนโรงแรมที่ MANAGE จำนวนกี่โรงแรม
-
-SELECT HOTEL_MANAGER.UserID, ALL_USER.UserName, COUNT(HOTEL_BRANCH.BranchID) AS ManagedBranches
-FROM HOTEL_MANAGER
-JOIN HOTEL ON HOTEL_MANAGER.UserID = HOTEL.UserID
-LEFT JOIN HOTEL_BRANCH ON HOTEL.HotelID = HOTEL_BRANCH.HotelID
-JOIN ALL_USER ON HOTEL_MANAGER.UserID = ALL_USER.UserID
-GROUP BY HOTEL_MANAGER.UserID,ALL_USER.UserName
-ORDER BY ManagedBranches DESC;
-
-
 -- top 5 hotels with an average rating of at least 4,
 -- categorizing them by revenue level based on booking amounts and counting the number of unique customers per hotel
 SELECT 
@@ -17,8 +6,8 @@ SELECT
     SUM("room"."pricenormal" * "booking"."numberofbooking") AS "totalrevenue",
     COUNT(DISTINCT "booking"."userid") AS "uniquecustomers",
     CASE 
-        WHEN SUM("room"."pricenormal" * "booking"."numberofbooking") > 10000 THEN 'High Revenue'
-        WHEN SUM("room"."pricenormal" * "booking"."numberofbooking") > 5000 THEN 'Medium Revenue'
+        WHEN SUM("room"."pricenormal" * "booking"."numberofbooking") > 4000 THEN 'High Revenue'
+        WHEN SUM("room"."pricenormal" * "booking"."numberofbooking") > 1500 THEN 'Medium Revenue'
         ELSE 'Low Revenue'
     END AS "revenuecategory"
 FROM 
